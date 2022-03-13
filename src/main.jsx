5168ea266a190d3897ac90aeaaafe5cc7f0a738f
RutaTang <953259325@qq.com>
2022-03-13 13:18:23 +0800

    add neoformt for format file

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Amplify, { Auth } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
 
-import Home from './pages/Home'
-import SignUp from './pages/SignUp'
-import LogIn from './pages/LogIn'
-import LogOut from './pages/LogOut'
-import {AuthProvider,AuthProtector} from './contexts/AuthContext'
-import {StateProvider} from './contexts/StateContext'
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import LogOut from "./pages/LogOut";
+import { AuthProtector, AuthProvider } from "./contexts/AuthContext";
+import { StateProvider } from "./contexts/StateContext";
 
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
-	<StateProvider>
-		<AuthProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<AuthProtector><Home/></AuthProtector>} />
-					<Route path="/signup" element={<SignUp />} />
-					<Route path="/login" element={<LogIn />} />
-					<Route path="/logout" element={<LogOut />} />
-				</Routes>
-			</BrowserRouter>
-		</AuthProvider>
-	</StateProvider>,
-	document.getElementById('root')
-)
+  <StateProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <AuthProtector>
+                <Home />
+              </AuthProtector>
+            }
+          />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/logout" element={<LogOut />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </StateProvider>,
+  document.getElementById("root"),
+);
