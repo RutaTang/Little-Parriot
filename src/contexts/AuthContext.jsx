c65d11f33e78a005c157be3bf2a46559ad712b42
RutaTang <953259325@qq.com>
2022-03-18 20:15:41 +0800

    uodate

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,51 +1,50 @@
-import {createContext,useContext} from 'react'
-import {useState, useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
+import { createContext, useContext } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AuthContext =  createContext()
+const AuthContext = createContext();
 
-const AuthProvider = (props)=>{
-    const [user,setUser] = useState(null)
-	const _user = localStorage.getItem("user")
-	useEffect(()=>{
-		if(_user !== null){
-			setUser(_user)
-		}
-	})
-	return (
-		<AuthContext.Provider value={{user,setUser}}>
-			{props.children}
-		</AuthContext.Provider>
-	)
-} 
+const AuthProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const _user = localStorage.getItem("user");
+  useEffect(() => {
+    if (_user !== null) {
+      setUser(_user);
+    }
+  });
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
-const AuthProtector = (props)=>{
-	const redirectTo = props.redirectTo ?? '/signup'
-	const navigate = useNavigate()
-	const {user,_} = useContext(AuthContext)
-	useEffect(()=>{
-		if(!user){
-			setTimeout(()=>{navigate(redirectTo)},500)
-		}
-	})
-	return (
-		<>
-			{
-			user ? props.children : <h1>Navigate to {redirectTo} </h1>
-			}
-		</>
-	)
-}
+const AuthProtector = (props) => {
+  const redirectTo = props.redirectTo ?? "/signup";
+  const navigate = useNavigate();
+  const { user, _ } = useContext(AuthContext);
+  useEffect(() => {
+    if (!user) {
+      setTimeout(() => {
+        navigate(redirectTo);
+      }, 500);
+    }
+  });
+  return (
+    <>
+      {user ? props.children : <h1>Navigate to {redirectTo}</h1>}
+    </>
+  );
+};
 
-const AuthWrapper = (props)=>{
-	return (
-		<AuthProvider>
-			<AuthProtector>
-				{props.children}
-			</AuthProtector>
-		</AuthProvider>
-	)
-}
+const AuthWrapper = (props) => {
+  return (
+    <AuthProvider>
+      <AuthProtector>
+        {props.children}
+      </AuthProtector>
+    </AuthProvider>
+  );
+};
 
-
-export {AuthProvider,AuthProtector,AuthContext, AuthWrapper}
+export { AuthContext, AuthProtector, AuthProvider, AuthWrapper };
