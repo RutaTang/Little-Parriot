379c1d8153bc2bf372ef88dc16ebb2a76804b02d
RutaTang <953259325@qq.com>
2022-03-30 00:53:53 +0800

    remove useEffect to avoid possible inf loop

--- src/pages/TestApi.jsx
+++ src/pages/TestApi.jsx
@@ -1,14 +1,11 @@
-import { useEffect,useState } from "react";
+import { useState } from "react";
 import { getAllStories } from "../backend/story/get-story";
 
 const TestApi = () => {
   const [json, setJson] = useState();
-  useEffect(() => {
-    getAllStories().then((jsonTest) => {
-      setJson(JSON.stringify(jsonTest, null, 2));
-    });
-  }, [json]);
-
+  getAllStories().then((jsonTest) => {
+    setJson(JSON.stringify(jsonTest, null, 2));
+  });
   return <pre>{json}</pre>;
 };
 
