a78fba0cc6c4240a0a79867f6b7ae34b2e651493
ruta <ruta@rutas-MacBook-Pro.local>
2022-04-03 15:48:49 +0800

    Finish UI Part for Voice page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,11 +31,29 @@ const FAKE_STORY_TAGS = [
   },
 ];
 
+const FAKE_VOICE_IEMS = [
+  {
+    id: 1,
+    name: "Women",
+    description: "A soft and kind voice with your sleep",
+    url: "/Mom.svg",
+    price: "free",
+  },
+  {
+    id: 2,
+    name: "Men",
+    description: "A magnetic voice follow your day, you will love it",
+    url: "/Dad.svg",
+    price: "free",
+  },
+];
+
 const Home = () => {
   const { page, setPage } = useContext(StateContext);
   const { playFullScreen, setPlayFullScreen } = useContext(StateContext);
   const [storyTags, setStoryTags] = useState();
-	const [currentTag, setCurrentTag] = useState(0);
+  const [currentTag, setCurrentTag] = useState(0);
+  const [currentVoiceId, setCurrentVoiceId] = useState(0);
 
   useEffect(() => {
     setStoryTags(FAKE_STORY_TAGS);
@@ -43,7 +61,7 @@ const Home = () => {
 
   return (
     <div className="w-screen pb-40 md:pb-0">
-      <Nav  />
+      <Nav />
       <Play />
       {/*Story*/}
       <div
@@ -55,7 +73,22 @@ const Home = () => {
         </h1>
         <div className="flex w-full md:flex-row flex-col">
           <div className="flex flex-col gap-y-5 md:mt-10 mt-3 md:w-1/5 w-full text-center md:text-left">
-            {storyTags && storyTags.map((tag,idx) => <p key={tag.name+idx} onClick={()=>{setCurrentTag(idx)}} className={currentTag === idx ? `px-3 py-1 bg-orange-300 text-white rounded-lg cursor-pointer` : 'cursor-pointer' }>{tag.name}</p>)}
+            {storyTags &&
+              storyTags.map((tag, idx) => (
+                <p
+                  key={tag.name + idx}
+                  onClick={() => {
+                    setCurrentTag(idx);
+                  }}
+                  className={
+                    currentTag === idx
+                      ? `px-3 py-1 bg-orange-300 text-white rounded-lg cursor-pointer`
+                      : "cursor-pointer"
+                  }
+                >
+                  {tag.name}
+                </p>
+              ))}
           </div>
           <div className="md:w-4/5 w-full md:grid-cols-3 md:mt-0 mt-10 md:grid md:mx-5 md:gap-x-8 md:gap-y-12">
             {/* Story Card  */}
@@ -86,8 +119,37 @@ const Home = () => {
         </div>
       </div>
       {/* Voice */}
-      <div style={{ display: `${page === 1 ? "block" : "none"}` }}>
-        Voice{page}
+      <div
+        className="w-screen h-screen mt-32"
+        style={{ display: `${page === 1 ? "block" : "none"}` }}
+      >
+        <div className="flex flex-wrap md:flex-row flex-col items-center w-[90%] gap-10 mx-auto">
+          {FAKE_VOICE_IEMS.map((item, id) => (
+            <div
+              className={`w-[300px] shadow shadow-gray-200 p-5 rounded-lg transition hover:scale-105 cursor-pointer ${
+                currentVoiceId === id ? "border-red-400 border-4" : ""
+              }`}
+							onClick={() => {setCurrentVoiceId(id)}}
+							key={item.name + id}
+            >
+              <img className="w-[160px] mx-auto" src={item.url} alt="" />
+              <h3 className="text-2xl text-black mt-2">{item.name}</h3>
+              <p className="text-gray-500">{item.description}</p>
+              <div className="flex justify-between items-center">
+                <p className="text-red-500">{item.price}</p>
+                <button
+                  className={`py-1 px-2 text-orange-400 ${
+                    currentVoiceId === id
+                      ? "bg-orange-400 rounded-xl text-white"
+                      : ""
+                  }`}
+                >
+                  {currentVoiceId === id ? "Current Voice" : "Set Default"}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
